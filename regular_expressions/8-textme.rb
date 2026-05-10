096c8ab79c14cd7be6aa5927234e1757dc86838e
Ange-Liana <l.batsinde@alustudent.com>
2026-05-10 02:02:41 -0700

    file 8

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-sender = ARGV[0].scan(/(?<=from:).*?(?=\])/).join
-receiver = ARGV[0].scan(/(?<=to:).*?(?=\])/).join
-flags = ARGV[0].scan(/(?<=flags:).*?(?=\])/).join
-puts "#{sender},#{receiver},#{flags}"
+Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
+List = [Format[0].compact, Format[1].compact, Format[2].compact]
+puts List.join(',')

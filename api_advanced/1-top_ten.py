33736f3b198221801aafd7ffb30b7a7b8a9939e8
Ange-Liana <l.batsinde@alustudent.com>
2026-07-10 13:47:55 -0700

    api advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,20 +7,27 @@ import requests
 
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "MyRedditAPIClient/1.0 (by /u/alu_student)"}
+    if not subreddit or not isinstance(subreddit, str):
+        print(None)
+        return
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {
+        "User-Agent": "Googlebot/2.1 (+http://www.google.com/bot.html)"
+    }
+    params = {"limit": 10}
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
         if response.status_code == 200:
-            data = response.json().get("data", {})
-            posts = data.get("children", [])
-            if not posts:
-                print(None)
-                return
-            for post in posts:
-                print(post.get("data", {}).get("title"))
-        else:
-            print(None)
+            data = response.json().get("data")
+            if data:
+                children = data.get("children")
+                if children and len(children) > 0:
+                    for post in children:
+                        print(post.get("data", {}).get("title"))
+                    return
+        print(None)
     except Exception:
         print(None)
